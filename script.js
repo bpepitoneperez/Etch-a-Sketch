0e901cf1af1d2ef4d6251e6a35662296bcd30bb9
Bryan Pepitone <brryann21@gmail.com>
2021-07-20 17:04:00 -0400

    Tons of progress. Grid is working, colors are working, resizing is working, clearing is working. Need to add more color options and update the styling/layout.

--- script.js
+++ script.js
@@ -1,15 +1,85 @@
-const container = document.querySelector('#grid-container');
+let pixelColor = '#656565';
+let random = false;
+let defaultBG = 'Gainsboro';
 
-let divArr = new Array(16);
-for (let i = 0; i < 16; i++) {
-    divArr[i] = new Array(16);
-    for (let j = 0; j < 16; j ++) {
-        divArr[i, j] = document.createElement('div');
-        divArr[i, j].style.width = '6%';
-        divArr[i, j].style.height = '0';
-        divArr[i, j].style.paddingBottom = '6%';
-        divArr[i, j].style.border = 'solid';
-        divArr[i, j].style.borderWidth = '1px';
-        container.appendChild(divArr[i, j]);
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearGrid);
+
+const blueButton = document.querySelector('#blue');
+blueButton.addEventListener('click', blueFunc);
+
+const randomButton = document.querySelector('#random');
+randomButton.addEventListener('click', randomChanger);
+
+
+
+//Create default grid
+createGrid(50);
+let currentSize = 50;
+
+function draw (color, square) {
+    square.style.backgroundColor = color;
+}
+
+const slider = document.querySelector('#myRange');
+slider.addEventListener('change', function(e) {
+    if (this.value * 4 != currentSize) {
+        resizeGrid(this.value * 4);
     }
+});
+
+function createGrid(size) {
+    const container = document.querySelector('#grid-container');
+    let maxWidth = container.clientWidth;
+    console.log(maxWidth);
+    let pixelSize = (100 / size + "%");
+    console.log(pixelSize);
+    for (let i = 0; i < size*size; i++) {
+        const currDiv = document.createElement('div');
+        currDiv.style.width = pixelSize;
+        currDiv.style.height = pixelSize;
+        //currDiv.style.borderInline = 'solid';
+        //currDiv.style.borderWidth = '1px';
+        currDiv.style.padding = '0';
+        currDiv.style.backgroundColor = defaultBG;
+        currDiv.setAttribute("name", i);
+        currDiv.setAttribute("class", "pixels")
+        currDiv.addEventListener('mouseover', function(e) {
+            if (random) {
+                pixelColor = randomColor();
+            }
+            draw(pixelColor, currDiv);
+            e.stopPropagation();
+        });
+        container.appendChild(currDiv);
+    }
+}
+
+function clearGrid() {
+    const pixels = document.querySelectorAll('.pixels');
+    pixels.forEach(pixel => pixel.style.backgroundColor = defaultBG);
+}
+
+function resizeGrid(newSize) {
+    const pixels = document.querySelectorAll('.pixels');
+    pixels.forEach(e => e.parentNode.removeChild(e));
+    createGrid(newSize);
+    currentSize = newSize;
+}
+
+function blueFunc() {
+    pixelColor = 'blue';
+}
+
+function randomChanger() {
+    random = !random;
+    console.log(random);
+}
+
+function randomColor() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
